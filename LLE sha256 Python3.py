9c9ab238736260bd73a9aa55760c9cfb52fc155a
Greagar <98690173+Greagar@users.noreply.github.com>
2022-05-01 16:41:02 +0200

    LLE with python3, on "sha256"
    Implementation using python to LLE

---
+++ LLE sha256 Python3.py
@@ -0,0 +1,21 @@
+# Layer-on-Layer-Encryption (LLE) 
+
+# LLE with sha256 Using python3
+
+from hashlib import sha256
+
+inp = input("Enter something : ")   # Input 
+
+hashSha256 = sha256(inp.encode('utf-8')).hexdigest()
+
+keyLength = int(input("Enter key length : "))   # How many times to loop
+x = 0
+
+# Looping as many times as keyLength
+while (x < keyLength):
+
+    hashSha256 = sha256(hashSha256.encode('utf-8')).hexdigest()
+    x = x+1
+
+# Printing final hash value
+print(hashSha256)
